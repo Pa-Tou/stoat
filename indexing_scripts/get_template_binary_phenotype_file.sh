a8157840a48f6b13fda612759ae8a86979dc360e
Xian Hui Chang <xian-hui.chang@inrae.fr>
2025-10-03 16:25:56 +0200

    Add script to make a template binary phenotype file

---
+++ indexing_scripts/get_template_binary_phenotype_file.sh
@@ -0,0 +1,8 @@
+# This takes as input a graph and writes a template binary phenotype file to stdout
+# The template will have all sample names with phenotype 1
+
+GRAPH=$1
+
+printf "FID\tIID\tPHENO\n" 
+vg paths -M -x $GRAPH | tail -n +2 | awk '{ if ( $3 == "NO_SAMPLE_NAME") {print $5} else {print $3}}' | sed -r 's/(.*)/\1\t\1\t1/g'
+
