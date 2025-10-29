64bb9e3c10475588180550ebd3e650d46f7bc541
Xian Hui Chang <xian-hui.chang@inrae.fr>
2025-10-29 16:22:01 +0100

    Put popping a chain behind omp critical

--- src/partitioner.cpp
+++ src/partitioner.cpp
@@ -290,9 +290,15 @@ void SnarlTraverserAndPartitioner::for_each_snarl_partition(const handlegraph::P
             // The actual while loop is run on a single thread
             #pragma omp single
             {
+                // I think it is fine to check if chains is empty outside of a omp critical barrier since it isn't changing it, and it waits at the end of the loop
+                // for all threads to finish if chains was empty
                 while (!chains.empty()) {
-                    handlegraph::net_handle_t chain = chains.back();
+                    handlegraph::net_handle_t chain;
+                    #pragma omp critical
+                    {
+                    chain = chains.back();
                     chains.pop_back();
+                    }
 
                     // Everything in here is parallelized
                     #pragma omp task
@@ -354,7 +360,7 @@ void SnarlTraverserAndPartitioner::for_each_snarl_partition(const handlegraph::P
                     if (chains.empty()) {
                         // Wait for tasks to complete
                         #pragma omp taskwait
-                    }
+                    } 
                 }// end while loop
             }// End omp single
         }//end omp shared
