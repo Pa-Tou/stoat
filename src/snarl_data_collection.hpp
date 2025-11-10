c6622b0d66141865a39d64608d59c14bf24f3891
Xian Hui Chang <xian-hui.chang@inrae.fr>
2025-11-10 17:07:04 +0100

    Add interface for new snarl data collection

--- src/snarl_data_collection.hpp
+++ src/snarl_data_collection.hpp
@@ -12,39 +12,85 @@ using namespace stoat;
 
 namespace stoat {
 
-// This holds snarl information from the distance index and graph: id of the snarl, reference coordinates, walks, and (optionally) partitions of embedded paths
-struct snarl_data_t {
+/// A class for holding per-snarl data
+/// Build the collection either from the distance index or by loading from a file
+/// Access snarls by chromosome name or all at once
+class SnarlDataCollection {
+
+
+
+    ///////////////////////////////////////////////////////// Public struct used for storing snarl information
     public:
-        // Constructor definition
-        snarl_data_t() {};
 
-        // Constructor from a snarl, calculate everything from the graph and distance index 
-        snarl_data_t(bdsg::net_handle_t snarl, const handlegraph::PathPositionHandleGraph& graph, const bdsg::SnarlDistanceIndex& distance_index, bool find_partitions=false);
+    // This holds snarl information from the distance index and graph: id of the snarl, reference coordinates, walks, and (optionally) partitions of embedded paths
+    struct snarl_data_t {
+        public:
+            // Constructor definition
+            snarl_data_t() {};
+    
+            // Constructor from a snarl, calculate everything from the graph and distance index 
+            snarl_data_t(bdsg::net_handle_t snarl, const handlegraph::PathPositionHandleGraph& graph, const bdsg::SnarlDistanceIndex& distance_index, bool find_partitions=false);
+    
+            // Constructor from elements
+            snarl_data_t(std::vector<std::string> type_variants, std::vector<Path_traversal_t> snarl_paths, stoat::Node_traversal_t start_node,
+                         stoat::Node_traversal_t end_node, std::string ref_path, size_t start_position, size_t end_position, size_t depth,
+                         std::vector<std::set<sample_hap_t>> partitions) :
+                         type_variants(type_variants), snarl_paths(snarl_paths), start_node(start_node), end_node(end_node), 
+                         ref_path(ref_path), start_position(start_position), end_position(end_position), depth(depth), partitions(partitions)
+                         {};
+    
+            // Type variant for each path: either 0 for deletion, length of the path, or min/max length of the path
+            // TODO: Change name to path_lengths or something
+            std::vector<std::string> type_variants;
+            // All possible walks through the snarl
+            std::vector<Path_traversal_t> snarl_paths;
+            // Start and end nodes, both pointing into the snarl
+            stoat::Node_traversal_t start_node;
+            stoat::Node_traversal_t end_node;
+    
+            std::string ref_path; //TODO: I think this could get pretty big, might want to save it as an index into a list of reference paths
+            // Start and end offset along the reference path
+            size_t start_position;
+            size_t end_position;
+            size_t depth;
+    
+            std::vector<std::set<sample_hap_t>> partitions;
+    };
 
-        // Constructor from elements
-        snarl_data_t(std::vector<std::string> type_variants, std::vector<Path_traversal_t> snarl_paths, stoat::Node_traversal_t start_node,
-                     stoat::Node_traversal_t end_node, std::string ref_path, size_t start_position, size_t end_position, size_t depth,
-                     std::vector<std::set<sample_hap_t>> partitions) :
-                     type_variants(type_variants), snarl_paths(snarl_paths), start_node(start_node), end_node(end_node), 
-                     ref_path(ref_path), start_position(start_position), end_position(end_position), depth(depth), partitions(partitions)
-                     {};
+    public:
+        ////////////////////////////////////////////// Public functions for building and interrogating snarl info
 
-        // Type variant for each path: either 0 for deletion, length of the path, or min/max length of the path
-        // TODO: Change name to path_lengths or something
-        std::vector<std::string> type_variants;
-        // All possible walks through the snarl
-        std::vector<Path_traversal_t> snarl_paths;
-        // Start and end nodes, both pointing into the snarl
-        stoat::Node_traversal_t start_node;
-        stoat::Node_traversal_t end_node;
+        /// Fill in the SnarlDataCollection for all snarls in the distance index
+        SnarlDataCollection(const handlegraph::PathPositionHandleGraph& graph, const bdsg::SnarlDistanceIndex& distance_index,
+                            size_t snarl_length_limit, size_t snarl_child_limit);
 
-        std::string ref_path; //TODO: I think this could get pretty big, might want to save it as an index into a list of reference paths
-        // Start and end offset along the reference path
-        size_t start_position;
-        size_t end_position;
-        size_t depth;
+        /// Run iteratee for all snarls with the given chr_name
+        void for_each_snarl_in_chromosome(const std::string& chr_name, const std::function<void(const snarl_data_t& snarl_info)>& iteratee) const;
 
-        std::vector<std::set<sample_hap_t>> partitions;
+        /// Run iteratee for all snarls
+        void for_each_snarl(const std::function<void(const snarl_data_t& snarl_info)>& iteratee) const;
+
+        /// Write the collection of snarls (as map from reference chromosome name to the snarl data on that path) to the given file
+        void write_snarl_data_collection(const string& filename) const;
+        
+        /// Load the collection of snarls (as map from reference chromosome name to the snarl data on that path) from the given file
+        void load_snarl_data_collection(const string& filename); 
+
+    //////////////////////////////////////////// Private data members
+
+    private:
+        /// This holds the snarl data as a map from the chromosome name to the data
+        //TODO: Make sure that this gets the chr name the way Matis did it
+        std::unordered_map<std::string, std::vector<snarl_data_t>> chr_to_snarl_data;
+
+    ////////////////////////////////////////// Private helper functions
+    
+    private:
+
+        /// Go through all the snarls in the distance index and fill in a map from reference chromosome name to the snarl data on that path
+        void get_all_snarl_data(const handlegraph::PathPositionHandleGraph& graph, const bdsg::SnarlDistanceIndex& distance_index,
+                                                                                  size_t snarl_length_limit, size_t snarl_child_limit);
+    
 };
 }
 
