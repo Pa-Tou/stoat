ec0b28d6cfa9772b712153e4e6e80912b86da83c
Xian Hui Chang <xian-hui.chang@inrae.fr>
2026-03-16 16:41:43 +0100

    Add machinery to check equality of genotype file for testing

--- tests/system/load_tables.cpp
+++ tests/system/load_tables.cpp
@@ -8,6 +8,254 @@
 #include <vector>
 #include <string>
 
+bool snarl_genotype_values_t::operator==(const snarl_genotype_values_t& other) const {
+    if (start_node != other.start_node ||
+        end_node != other.end_node ||
+        reference != other.reference ||
+        start_offset != other.start_offset ||
+        end_offset != other.end_offset ||
+        depth != other.depth ||
+        allele_lengths.size() != other.allele_lengths.size() ||
+        walks.size() != other.walks.size() ||
+        sequences.size() != other.sequences.size() ||
+        sample_to_allele.size() != other.sample_to_allele.size()) {
+        return false;
+    }
+    // Check that the alleles are all the same.
+    // Alleles may be in a different order, so try to map from this object's alleles to the other's alleles
+    std::unordered_map<size_t, size_t> allele_conversion;
+
+
+    // Go through all i walks and all j other walks and see what matches, and make sure that everything has a match
+    if (walks.size() != 0) {
+        std::vector<bool> found_j(other.walks.size() , false);
+        for (size_t i = 0 ; i < walks.size() ; i++) {
+            bool found_i = false;
+            for (size_t j = 0 ; j < other.walks.size() ; j++) {
+                if (walks[i] == other.walks[j]) {
+                    found_i = true;
+                    if (found_j[j]) {
+                        std::cerr << "Duplicate walk? " << other.walks[j] << std::endl;
+                    }
+                    found_j[j] = true;
+                    allele_conversion[i] = j;
+                }
+            }
+            if (!found_i) {
+                std::cerr << "Walk not found in other snarl genotype: " << walks[i] << std::endl;
+                return false;
+            }
+        }
+        for (size_t j= 0 ; j < other.walks.size() ; j++) {
+            if (!found_j[j]) {
+                std::cerr << "Walk  not found in this snarl genotype: " << other.walks[j] << std::endl;
+                return false;
+            }
+        }
+    }
+
+    // Check the sequences
+    if (allele_conversion.size() == 0) {
+        // If we didn't already get the conversion, try again with the sequences
+        std::vector<bool> found_j(other.sequences.size() , false);
+        for (size_t i = 0 ; i < sequences.size() ; i++) {
+            bool found_i = false;
+            for (size_t j = 0 ; j < other.sequences.size() ; j++) {
+                if (sequences[i] == other.sequences[j]) {
+                    found_i = true;
+                    if (found_j[j]) {
+                        std::cerr << "Duplicate sequences? " << other.sequences[j] << std::endl;
+                    }
+                    found_j[j] = true;
+                    allele_conversion[i] = j;
+                }
+            }
+            if (!found_i) {
+                std::cerr << "Sequence not found in other snarl genotype: " << sequences[i] << std::endl;
+                return false;
+            }
+        }
+        for (size_t j= 0 ; j < other.sequences.size() ; j++) {
+            if (!found_j[j]) {
+                std::cerr << "Sequence  not found in this snarl genotype: " << other.sequences[j] << std::endl;
+                return false;
+            }
+        }
+    } else {
+        for (size_t i = 0 ; i < sequences.size() ; i++) {
+            if (sequences[i] != other.sequences[allele_conversion[i]]) {
+                return false;
+            }
+        }
+    }
+
+    // Check the lengths
+    if (allele_conversion.size() == 0) {
+
+        // If we didn't get the conversion already then sort the lengths and check if they are the same
+        std::vector<size_t> sorted_lengths = allele_lengths;
+        std::sort(sorted_lengths.begin(), sorted_lengths.end());
+        std::vector<size_t> other_sorted_lengths = other.allele_lengths;
+        std::sort(other_sorted_lengths.begin(), other_sorted_lengths.end());
+
+        for (size_t i = 0 ; i < sorted_lengths.size() ; i++ ){
+            if (sorted_lengths[i] != other_sorted_lengths[i]) {
+                return false;
+            }
+        }
+
+    } else {
+
+        for (size_t i = 0 ; i < allele_lengths.size() ; i++) {
+            if (allele_lengths[i] != other.allele_lengths[allele_conversion[i]]) {
+                return false;
+            }
+        }
+    }
+    for (const auto& x : sample_to_allele) {
+        if (x.second == std::numeric_limits<size_t>::max()) {
+            if (other.sample_to_allele.at(x.first) != std::numeric_limits<size_t>::max()) {
+                return false;
+            }
+        } else {
+            if (other.sample_to_allele.at(x.first) == std::numeric_limits<size_t>::max()) {
+                return false;
+            } else if (x.second != allele_conversion[other.sample_to_allele.at(x.first)]) {
+                return false;
+            }
+        }
+    } 
+
+    
+    return true;
+}
+
+
+std::vector<snarl_genotype_values_t> load_gnotype_file(const std::string& infile) {
+    std::ifstream instream;
+    instream.open(infile);
+
+    std::string line;
+
+    // First header line should be the version
+    std::getline(instream, line);
+    if (line != "#SNARL_DATA_v1.0" ) {
+        std::cerr << "Warning: genotype file is version " << line << std::endl;
+    }
+    // allele size limit
+    std::getline(instream, line);
+    // snarl child limit
+    std::getline(instream, line);
+    // walk steps limit 
+    std::getline(instream, line);
+    // Start of refs
+    std::getline(instream, line);
+
+    std::vector<std::string> references;
+    while (line != "#SNARLS") {
+        std::getline(instream, line);
+        std::string ref (line.begin()+1, line.end());
+        references.emplace_back(ref); 
+    }
+
+
+    //Next line is the header with the samples
+    std::getline(instream, line);
+    std::vector<std::string> sample_names;
+
+    std::stringstream linestream(line);
+    std::string sample_name;
+    // First go through the first 9 things and ignore them
+    for (size_t i = 0 ; i < 9 ; i++) {
+        std::getline(linestream, sample_name, '\t');
+    }
+    while (std::getline(linestream, sample_name, '\t')) {
+        sample_names.emplace_back(sample_name);
+    }
+
+    std::vector<snarl_genotype_values_t> snarls;
+
+    // Now go through the actual snarls
+    while (std::getline(instream, line)) {
+        snarl_genotype_values_t snarl;
+
+        std::stringstream snarlstream(line);
+
+        std::string part;
+        //Snarl start node traversal
+        std::getline(snarlstream, part, '\t');
+        snarl.start_node = part;
+        
+        //Snarl start node traversal
+        std::getline(snarlstream, part, '\t');
+        snarl.end_node = part;
+        
+        // Index of reference
+        std::getline(snarlstream, part, '\t');
+        snarl.reference = references.at(std::stoull(part));
+        
+        // Start offset along reference
+        std::getline(snarlstream, part, '\t');
+        snarl.start_offset = std::stoull(part);
+        
+        // End offset along reference
+        std::getline(snarlstream, part, '\t');
+        snarl.end_offset = std::stoull(part);
+        
+        // Snarl depth
+        std::getline(snarlstream, part, '\t');
+        snarl.depth = std::stoull(part);
+
+        // Lengths of paths
+        std::vector<size_t> lengths;
+        std::string length_str;
+        while (std::getline(snarlstream, length_str, ',')) {
+            lengths.emplace_back(std::stoull(length_str));
+        }
+        snarl.allele_lengths = std::move(lengths);
+
+        // Walks
+        std::vector<std::string> walks;
+        std::string path_str;
+        while (std::getline(snarlstream, path_str, ',')) {
+            walks.emplace_back(std::move(path_str));
+        }
+        snarl.walks = std::move(walks);
+
+
+        // Sequences
+        std::getline(snarlstream, part, '\t');
+        if (part != ".") {
+            std::vector<std::string> sequences;
+            std::stringstream seqstream(part);
+            std::string seq;
+            bool last_empty_sequence = part.size() > 0 && part.at(part.size()-1) == ',';
+            while (std::getline(seqstream, seq, ',')) {
+                sequences.emplace_back(seq);
+            }
+        
+            if (last_empty_sequence) {
+                sequences.emplace_back("");
+            }
+        
+            snarl.sequences = std::move(sequences);
+        }
+
+        std::unordered_map<std::string, size_t> sample_to_allele;
+        size_t sample_num = 0;
+        while (std::getline(snarlstream, part, '\t')) {
+            sample_to_allele[sample_names.at(sample_num)] = (part == "." ? std::numeric_limits<size_t>::max() : std::stoull(part));
+            sample_num ++;
+        };
+
+        snarl.sample_to_allele = std::move(sample_to_allele);
+    
+
+    }
+
+    instream.close();
+    return snarls;
+}
 bool binary_table_values_t::operator==(const binary_table_values_t& other) const {
     if (chr != other.chr ||
         start_pos != other.start_pos ||
--- tests/system/load_tables.hpp
+++ tests/system/load_tables.hpp
@@ -7,6 +7,29 @@
 #include <unordered_map>
 #include <regex>
 
+
+////////////////////////////////////////// snarl_genotype.tsv
+struct snarl_genotype_values_t {
+    std::string start_node;
+    std::string end_node;
+    std::string reference;
+    size_t start_offset;
+    size_t end_offset;
+    size_t depth;
+    std::vector<size_t> allele_lengths;
+    std::vector<std::string> walks;
+    std::vector<std::string> sequences;
+    std::unordered_map<std::string, size_t> sample_to_allele;
+
+    bool operator==(const snarl_genotype_values_t& other) const;
+};
+
+// Given the file name, load the snarl genotypes into a vector
+// Throw an error if the file seems bad
+std::vector<snarl_genotype_values_t> load_gnotype_file(const std::string& infile);
+
+
+/////////////////////////////////////////// Binary table from stoat test
 struct binary_table_values_t{
     std::string chr;
     size_t start_pos;
@@ -26,3 +49,5 @@ struct binary_table_values_t{
 //load info from a line from the binary tsv file 
 //CHR START_POS END_POS SNARL PATH_LENGTHS P_FISHER P_CHI2 GROUP_PATHS DEPTH
 binary_table_values_t load_binary_snarl_line(std::string);
+
+
