d67b82d6ddad1ac3136f718a26dda7db84f6ba3e
Xian Hui Chang <xian-hui.chang@inrae.fr>
2026-05-12 17:23:19 +0200

    Add test

--- tests/system/vcf_simu_test.cpp
+++ tests/system/vcf_simu_test.cpp
@@ -1266,3 +1266,181 @@ TEST_CASE("Output simple nested chain with many samples", "[detangle]") {
     fs::remove(reference_filename);
     fs::remove(vcf_filename);
 }
+TEST_CASE("Node vcf on simple bubble", "[node]") {
+    /*
+           1    
+         /   \
+        0--2--4
+         \   /
+           3
+
+    paths are called pathN
+    {0, 1, 4}, {0, 1, 4}, {0, 2, 4}, {0, 3, 4}
+    */
+
+
+
+    const std::string output_dir = "../output_binary";
+    const std::string graph_base = "../tests/test_data/test_graphs/simple_bubble";
+    const std::string samples_filename = "./samples.tsv";
+    const std::string reference_filename = "./references.tsv";
+    std::string vcf_filename = "./test.vcf";
+
+    std::ofstream vcf_out;
+    vcf_out.open(vcf_filename);
+    vcf_out << "##fileformat=VCFv4.2" << std::endl;
+    vcf_out << "##FILTER=<ID=PASS,Description=\"All filters passed\">" << std::endl;
+    vcf_out << "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">" << std::endl;
+    vcf_out << "##INFO=<ID=LV,Number=1,Type=Integer,Description=\"Level in the snarl tree (0=top level)\">" << std::endl;
+    vcf_out << "##INFO=<ID=AT,Number=R,Type=String,Description=\"Allele Traversal as path in graph\">" << std::endl;
+    vcf_out << "##contig=<ID=path0,length=100>" << std::endl;
+    vcf_out << "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tS1\tS2" << std::endl;
+    vcf_out << "path0\t0\t>1>5\tA\tG,C\t60\t.\tLV=0;AT=>1>2>5,>1>3>5,>1>4>5\tGT\t0/1\t0/2" << std::endl;
+    vcf_out.close();
+
+
+    std::vector<std::string> samples_of_interest = {"S1"};
+    std::vector<std::string> other_samples = {"S2"};
+
+    std::string write_cmd = "echo \"SAMPLE\tPHENO\" > " + samples_filename;
+    int ignore = std::system(write_cmd.c_str());
+    for (auto sample : samples_of_interest) {
+        write_cmd = "echo \"" + sample + "\t1\" >> " + samples_filename;
+        ignore = std::system(write_cmd.c_str());
+    }
+    for (auto sample : other_samples) {
+        write_cmd = "echo \"" + sample + "\t0\" >> " + samples_filename;
+        ignore = std::system(write_cmd.c_str());
+    }
+
+    // Write the reference file
+    write_cmd = "echo path0 > " + reference_filename;
+    ignore = std::system(write_cmd.c_str());
+
+    SECTION("Test without untangling") {
+        // Make the snarl file
+
+        std::string cmd = (std::string)"../bin/stoat vcf --mode node -u"
+            + " -g " + graph_base + ".hg"
+            + " -d " + graph_base + ".dist"
+            + " -R " + reference_filename
+            + " -v " + vcf_filename
+            + " -o " + output_dir;
+        std::cerr << "Run command " << cmd << std::endl;
+        int command_output = std::system(cmd.c_str());
+
+        if (command_output != 0) {
+            std::cerr << "Command failed: " << cmd << "\n";
+            REQUIRE(false);
+        }
+
+        stoat::BgzReader gt_reader(output_dir + "/node_info.tsv");
+        std::string line; 
+        // Get the index of each sample in the vcf (S1#0, S1#1, S2#0, S2#1)
+        std::vector<size_t> sample_index(4,0);
+        while (gt_reader.getline(line)) {
+            if (line.at(0) == '#') {
+                //skip the header except to get the samples
+                std::string header = "#NODE";
+                if (std::mismatch(header.begin(), header.end(), line.begin()).first == header.end() ) {
+                    // If this is the header line with the sample names
+                    std::stringstream headerstream(line);
+                    std::string first_node;
+                    std::getline(headerstream, first_node, '\t');
+                    // Get to the 10th item, which is the first genotype
+                    std::string item;
+                    std::getline(headerstream, item, '\t'); // got ref
+                    std::getline(headerstream, item, '\t'); // got pos
+                    std::getline(headerstream, item, '\t'); // got depth
+
+                    for (size_t i = 0 ; i < 4 ; i++) {
+                        std::getline(headerstream, item, '\t'); // sample name
+                        if (item == "S1#0") {
+                            sample_index[0] = i;
+                        } else if (item == "S1#1") {
+                            sample_index[1] = i;
+                        } else if (item == "S2#0") {
+                            sample_index[2] = i;
+                        } else if (item == "S2#1") {
+                            sample_index[3] = i;
+                        } else {
+                            std::cerr << "Unknown sample name " << item << std::endl;
+                            REQUIRE(false);
+                        }
+                    }
+                    REQUIRE(!std::getline(headerstream, item, '\t'));
+                }
+                continue;
+            }
+            // Get the start node, which is the first thing in the tab separated line
+            std::stringstream linestream(line);
+            std::string node_id;
+            std::getline(linestream, node_id, '\t');
+            // Get to the 5th item, which is the first genotype
+            std::string item;
+            std::getline(linestream, item, '\t'); // got ref
+            std::getline(linestream, item, '\t'); // got offset
+            std::getline(linestream, item, '\t'); // got depth
+
+            std::vector<std::string> genotypes;
+            std::string genotype;
+            while (std::getline(linestream, genotype, '\t')) {
+                genotypes.emplace_back(genotype);
+            }
+            if (node_id == "1") {
+
+                // Get the genotypes
+                REQUIRE(genotypes[sample_index[0]] == "1");
+                REQUIRE(genotypes[sample_index[1]] == "1");
+                REQUIRE(genotypes[sample_index[2]] == "1");
+                REQUIRE(genotypes[sample_index[3]] == "1");
+
+            } else if (node_id == "2") {
+
+                // Get the genotypes
+                REQUIRE(genotypes[sample_index[0]] == "1");
+                REQUIRE(genotypes[sample_index[1]] == "0");
+                REQUIRE(genotypes[sample_index[2]] == "1");
+                REQUIRE(genotypes[sample_index[3]] == "0");
+
+            } else if (node_id == "3") {
+
+                // Get the genotypes
+                REQUIRE(genotypes[sample_index[0]] == "0");
+                REQUIRE(genotypes[sample_index[1]] == "1");
+                REQUIRE(genotypes[sample_index[2]] == "0");
+                REQUIRE(genotypes[sample_index[3]] == "0");
+
+            } else if (node_id == "4") {
+
+                // Get the genotypes
+                REQUIRE(genotypes[sample_index[0]] == "0");
+                REQUIRE(genotypes[sample_index[1]] == "0");
+                REQUIRE(genotypes[sample_index[2]] == "0");
+                REQUIRE(genotypes[sample_index[3]] == "1");
+
+            } else if (node_id == "5") {
+
+                // Get the genotypes
+                REQUIRE(genotypes[sample_index[0]] == "1");
+                REQUIRE(genotypes[sample_index[1]] == "1");
+                REQUIRE(genotypes[sample_index[2]] == "1");
+                REQUIRE(genotypes[sample_index[3]] == "1");
+
+            } else {
+                // Snarl that shouldn't exist
+                std::cerr << "Node that shouldn't exist " << node_id << std::endl;
+                REQUIRE(false);
+            }
+        }
+
+        gt_reader.close();
+        clean_output_dir(output_dir);
+    }
+
+
+    //clean_output_dir(output_dir);
+    //fs::remove(samples_filename);
+    //fs::remove(reference_filename);
+    //fs::remove(vcf_filename);
+}
