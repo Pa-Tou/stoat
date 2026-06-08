6811dea97f172711ea182bb72fa6c026d6adbfc2
Xian Hui Chang <xian-hui.chang@inrae.fr>
2026-06-08 18:22:28 +0200

    Fix typo

--- src/snarl_data_collection.cpp
+++ src/snarl_data_collection.cpp
@@ -501,7 +501,7 @@ void SnarlDataCollection::for_each_snarl_in_file(stoat::Reader& in_reader, const
     load_snarl_data_collection_header(in_reader);
     std::string line; 
     while (in_reader.getline(line)) {
-        run_iteratee_on_snarl_data_line(l, iteratee);
+        run_iteratee_on_snarl_data_line(line, iteratee);
     }
 }
 
