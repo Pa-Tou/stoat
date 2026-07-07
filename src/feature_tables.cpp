6babce7f7f03508f7223226d04fd39fc181dd3b2
Jean Monlong <jean.monlong@gmail.com>
2026-07-07 16:40:49 +0200

    keep all samples even if no supporting alleles
    In the case of nested snarls, we used to keep and test only samples
    that had at least one allele traversing the snarl. To deal with
    potential association of the parent snarl, we had to track how many
    alleles each samples had using a *total_allele_count* covariable that
    was added to the model.
    
    This approach seemed to work and made sense, why include samples that
    have no allelic information in the model. However, there were some
    limitations: for some snarls, the number of samples tested could go
    down a lot and lead to unstable behaviors during model fitting. Also,
    it felt wrong that some snarls were tested using only a subset of
    samples.
    
    Including all samples in all tests should work better. We avoid
    situations with low sample sizes. Even in cases of rare nested snarls,
    the additional samples should help better model the effect of
    covariables (including the one about the total allele count). And that
    way, all tests are performed on the same set of samples which should
    make their combination/correction afterwards more correct. It might
    also make our life easier when implementing/using linear mixed models.
    
    Note: do we still need the -I parameter for `stoat test` then?

--- src/feature_tables.cpp
+++ src/feature_tables.cpp
@@ -363,15 +363,6 @@ void GenotypeTable::link_to_covariates(const CovariateTable& in_covariates) {
 
 void GenotypeTable::remove_noncovered_samples() {
 
-    // check the total allele count (filled previously) to decide if a sample should be masked
-    for (size_t samp_i = 0; samp_i < n_samples; samp_i++) {
-        // no allele counts present in sample or sample not have phenotype 
-        if (this->total_allele_counts_per_sample.at(samp_i) == 0 && !row_mask[samp_i]) {
-            row_mask[samp_i] = true;
-            n_active_samples--;
-        }
-    }
-
     // mask samples that don't have a phenotype
     size_t masked_samples = 0;
     if (linked_phenotype) {
